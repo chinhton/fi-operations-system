d271b5367e083efed9a84b2d40c253d6d2b8d124
chinhton <cton@fcimg.com>
2026-07-01 17:13:22 -0700

    Routine update

--- src/shipit.bat
+++ src/shipit.bat
@@ -1,4 +1,25 @@
 @echo off
+setlocal
+
+:: Clear any old messages
+set "msg="
+
+:: Ask for the commit message
+set /p msg="Enter commit message (or press Enter for 'Routine update'): "
+
+:: If nothing was typed, use the default
+if not defined msg set "msg=Routine update"
+
+echo.
+echo [1/3] Staging files...
 git add .
-git commit -m "Routine update"
+
+echo [2/3] Committing: "%msg%"
+git commit -m "%msg%"
+
+echo [3/3] Pushing to Azure...
 git push origin main
+
+echo.
+echo ✅ Shipped!
+endlocal
