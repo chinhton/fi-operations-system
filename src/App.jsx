b5840eefd99cd919014933d03f58d7299df06adc
chinhton <cton@fcimg.com>
2026-07-07 16:30:03 -0700

    removed execute button fron facility asset

--- src/App.jsx
+++ src/App.jsx
@@ -1639,7 +1639,6 @@ export default function App() {
                                     </div>
                                   </td>
                                   <td className="px-6 py-4 text-right space-x-4">
-                                    <button onClick={() => handleOpenPmModal(asset.id)} className="text-xs font-bold text-[#005596] hover:text-[#005596]/80 transition">Execute PM</button>
                                     {isSystemAdmin && (
                                       <button onClick={() => deleteAsset(asset.id)} className="text-xs font-bold text-red-600 hover:text-red-800 transition">Delete</button>
                                     )}
